3d7bb6db630f414ecdf5c6c26911d5e2eef3b8c3
Ajala Matthew Adekunle <symplymatt@gmail.com>
2025-09-04 22:34:23 +0100

    view all new

--- src/components/home/NewArrivals.tsx
+++ src/components/home/NewArrivals.tsx
@@ -8,7 +8,7 @@ const NewArrivals = () => {
     <div className="w-full flex flex-col items-center gap-[40px] tmd:gap-[20px] mb-[40px] overflow-visible">
         <div className="w-full flex flex-col tmd:flex-row items-center justify-between h-[48px] px-[20px] tmd:px-[50px] overflow-visible gap-[16px] text-center mb-[24px] tmd:mb-[24px]">
             <div className="text-[28px] tmd:text-[48px] font-normal leading-[32.78px] tracking-[0%] text-center">Featured</div>
-            <div className="h-[48px] bg-white border border-black px-[36px] py-[12px] cursor-pointer flex items-center justify-center font-semibold leading-[24px] tracking-[0%] whitespace-nowrap transition-transform duration-200 hover:scale-[0.9]">VIEW ALL NEW</div>
+            {/* <div className="h-[48px] bg-white border border-black px-[36px] py-[12px] cursor-pointer flex items-center justify-center font-semibold leading-[24px] tracking-[0%] whitespace-nowrap transition-transform duration-200 hover:scale-[0.9]">VIEW ALL NEW</div> */}
         </div>
         <GridSlider categoriesAndProducts={newArrivals}/>
     </div>
