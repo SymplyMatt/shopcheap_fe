7bb26f997e1a9c09f888bb88006d0a1704d19361
Ajala Matthew Adekunle <symplymatt@gmail.com>
2025-09-04 22:24:01 +0100

    baseurl

--- src/utils/utils.tsx
+++ src/utils/utils.tsx
@@ -243,7 +243,7 @@ function normalizeHeaders(
   );
 }
 
-const baseurl = 'http://localhost:1235/';
+const baseurl = 'https://grocery-errands.onrender.com/';
 export async function apiRequest(
   endpoint: string,
   method: Method = "GET",
